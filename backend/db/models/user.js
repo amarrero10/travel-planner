38cb254b9f2969457c2176a6b48ff3384c3ebdc4
Albert Marrero <albert.marrero10@gmail.com>
2024-07-14 22:28:00 -0400

    set up users table

---
+++ backend/db/models/user.js
@@ -0,0 +1,73 @@
+"use strict";
+const { Model, Validator } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      //     define association here
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+        },
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          len: [4, 100],
+          isNotEmail(value) {
+            if (!Validator.isEmail(value)) {
+              throw new Error("must be an email.");
+            }
+          },
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Must not be an email");
+            }
+          },
+        },
+      },
+      hashpass: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+    }
+  );
+  return User;
+};
