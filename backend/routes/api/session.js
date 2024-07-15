cd6b049cf971eae15d1803a52c3a16d50ca93342
Albert Marrero <albert.marrero10@gmail.com>
2024-07-14 23:35:59 -0400

    Added user function

---
+++ backend/routes/api/session.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+// Log in
+router.post("/", async (req, res, next) => {
+  const { credential, password } = req.body;
+  console.log(credential, password);
+
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  console.log(user);
+
+  if (!user || !bcrypt.compareSync(password, `${user.hashpass}`)) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
+module.exports = router;
---
+++ backend/routes/api/users.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+// Sign up
+router.post("/", async (req, res) => {
+  const { firstName, lastName, email, hashedPassword, username } = req.body;
+  console.log("PASSWORD", req.body);
+  const hashpass = bcrypt.hashSync(hashedPassword);
+  const user = await User.create({ firstName, lastName, email, username, hashpass });
+
+  const safeUser = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
+
+module.exports = router;
